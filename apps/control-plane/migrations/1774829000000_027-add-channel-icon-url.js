b047fc66d699bc9a1deec4ad2fdb87ad5fc5ac8e
Desmond Duval <haydre@gmail.com>
2026-04-04 18:51:43 -0400

    Database migration error fix

--- apps/control-plane/migrations/1774829000000_027-add-channel-icon-url.js
+++ apps/control-plane/migrations/1774829000000_027-add-channel-icon-url.js
@@ -1,9 +1,9 @@
-exports.up = (pgm) => {
+export const up = (pgm) => {
   pgm.addColumn("channels", {
     icon_url: { type: "text", notNull: false },
   });
 };
 
-exports.down = (pgm) => {
+export const down = (pgm) => {
   pgm.dropColumn("channels", "icon_url");
 };
